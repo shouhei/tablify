4902e7a617d309941d380992fe36ee21a0c6365e
山口将平 <shouhei.yamaguchi.be@gmail.com>
2016-03-17 16:52:31 +0900

    for simlink

--- init.sh
+++ init.sh
@@ -1,4 +1,11 @@
 #! /bin/bash
-cd `dirname $0`
+SCRIPT_HOME=$0
+
+if [ -n "`readlink $SCRIPT_HOME`" ] ; then
+    SCRIPT_HOME="`readlink $SCRIPT_HOME`"
+fi
+
+SCRIPT_HOME="`dirname $SCRIPT_HOME`"
+cd `dirname $SCRIPT_HOME`
 bower install
 cd -
--- update.sh
+++ update.sh
@@ -1,4 +1,11 @@
 #! /bin/bash
+SCRIPT_HOME=$0
+
+if [ -n "`readlink $SCRIPT_HOME`" ] ; then
+    SCRIPT_HOME="`readlink $SCRIPT_HOME`"
+fi
+
+SCRIPT_HOME="`dirname $SCRIPT_HOME`"
 cd `dirname $0`
 bower update
 cd -
