df430656b602ff12f9f63c48564dd5f755a5b5f4
山口将平 <shouhei.yamaguchi.be@gmail.com>
2016-03-17 17:09:58 +0900

    for simlink

--- update.sh
+++ update.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
 SCRIPT_HOME=$0
-
+cd `dirname $0`
 if [ -n "`readlink $SCRIPT_HOME`" ] ; then
     SCRIPT_HOME="`readlink $SCRIPT_HOME`"
 fi
 
 SCRIPT_HOME="`dirname $SCRIPT_HOME`"
-cd `dirname $0`
+cd $SCRIPT_HOME
+SCRIPT_HOME=`pwd`
+cd $SCRIPT_HOME
 bower update
-cd -
