9c76468f3643d62ad97f1ad17f4cedfcbd85b1d6
山口将平 <shouhei.yamaguchi.be@gmail.com>
2016-03-17 18:24:46 +0900

    add tyr catch

--- app.js
+++ app.js
@@ -9,12 +9,23 @@ var data = fs.readFileSync(file).toString();
 var path = __dirname
 var template = _.template(fs.readFileSync(path + "/templates/index.html").toString());
 
-parse(data, {}, function(err, output){
-  if(err === null){
+
+try {
+  main();
+} catch(e) { //try内でエラーおきたらこっち
+  process.stdout.write(e);
+  process.exit(1);
+}
+
+function main(){
+
+  parse(data, {}, function(err, output){
+    if(err === null){
       console.log(template({path: path, header: output[0], body: output.slice(1)}));
-  }else{
-    process.stdout.write(err);
-    process.exit(1);
-  }
-});
+    }else{
+      process.stdout.write(err);
+      process.exit(1);
+    }
+  });
+}
 process.exit(0);
